77cccddcf4c17b644228a73e676cbd709f968a7d
unknown <leanhphuong405@gmail.com>
2021-10-21 09:40:23 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,35 @@
+﻿ $(function(){
+   $('.n1').click(function(){
+      $('html').animate({scrollTop:$('.anh3').offset().top},1000);
+      return false;
+   })
+
+   $('.n2').click(function(){
+      $('html').animate({scrollTop:$('.anh4').offset().top},1000);
+      return false;
+   })
+
+   $('.n3').click(function(){
+      $('html').animate({scrollTop:$('.anh5').offset().top},1000);
+      return false;
+   })
+
+    $(window).scroll(function(){
+       var vitri = $('html').scrollTop();
+       if(vitri > 305){
+        $('.navbar-fixed-top').addClass('them');
+      }
+       else if(vitri < 305){
+        $('.navbar-fixed-top').removeClass('them');
+      }
+      if(vitri > 500){
+         $('.navbar-default').removeClass('navbar-fixed-top');
+      }
+      else{
+         $('.navbar-default').addClass('navbar-fixed-top');
+      }
+
+      $('[hieuung = "popover"]').popover();
+
+    })
+ })
